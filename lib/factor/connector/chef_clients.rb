fcae3557a70998cd449ae45ff0eb8bd8d850ed21
Maciej Skierkowski <maciej@skierkowski.com>
2015-01-21 13:16:58 -0800

    Add chef::client::create

--- lib/factor/connector/chef_clients.rb
+++ lib/factor/connector/chef_clients.rb
@@ -81,4 +81,50 @@ Factor::Connector.service 'chef_clients' do
 
     action_callback content.to_hash
   end
+
+  action 'create' do |params|
+    organization = params['organization']
+    chef_server  = params['chef_server']
+    client_name  = params['client_name']
+    key          = params['client_key']
+
+    fail 'Client Name (client_name) is required' unless client_name
+    fail 'Private Key (client_key) is required' unless key
+    fail 'Organization (organization) or Chef Server URL (chef_server) is required' unless organization || chef_server
+    fail 'Organization (organization) or Chef Server URL (chef_server) is required, but not both' if organization && chef_server
+    fail 'New Client Name (name) is required' unless params['name']
+
+    chef_server ||= "https://api.opscode.com/organizations/#{organization}"
+
+    info 'Setting up private key'
+    begin
+      private_key_file = Tempfile.new('private')
+      private_key_file.write(key)
+      private_key_file.close
+    rescue
+      fail 'Failed to setup private key'
+    end
+
+    connection_settings = {
+      endpoint: chef_server,
+      client:   client_name,
+      key:      private_key_file.path,
+    }
+
+    available_params = %w(name admin public_key private_key validator)
+    client_params = {}
+    available_params.each do |param|
+      client_params[param.to_sym] = params[param] if params.include?(param)
+    end
+
+    begin
+      info "Creating new client '#{client_params['name']}'"
+      chef = ChefAPI::Connection.new connection_settings
+      content = chef.clients.create(client_params)
+    rescue => ex
+      fail ex.message
+    end
+
+    action_callback content.to_hash
+  end
 end
--- spec/chef_clients_spec.rb
+++ spec/chef_clients_spec.rb
@@ -46,5 +46,21 @@ describe 'chef' do
         expect(content).to include(:validator)
       end
     end
+
+    it ':: create' do
+      client_name = "test-#{SecureRandom.hex(4)}"
+      params = @params.merge('name'=>client_name)
+      @service_instance.test_action('create',params) do
+        content = expect_return[:payload]
+        expect(content).to be_a(Hash)
+        expect(content).to include(:name)
+        expect(content[:name]).to eq(client_name)
+        expect(content).to include(:admin)
+        expect(content).to include(:public_key)
+        expect(content).to include(:private_key)
+        expect(content).to include(:validator)
+      end
+      chef.clients.fetch(client_name).destroy
+    end
   end
 end
