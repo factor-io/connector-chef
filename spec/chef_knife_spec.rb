ea063eb8af8c1825ea9d8983d6527a49259336c1
Maciej Skierkowski <maciej@skierkowski.com>
2015-01-21 12:48:17 -0800

    Run test only if required ENVs are set

--- spec/chef_knife_spec.rb
+++ spec/chef_knife_spec.rb
@@ -6,24 +6,21 @@ describe 'chef' do
       @service_instance = service_instance('chef_knife')
     end
 
-    it ':: bootstrap' do
+    if ENV['CHEF_TEST_HOST'] && ENV['CHEF_TEST_NAME'] && ENV['CHEF_TEST_RUNLIST']
 
-      params = {
-        'host' => 'root@sandbox.factor.io',
-        'private_key' => File.read(File.expand_path('~/Dropbox/Factor/Dev/deployment.pem')),
-        'validation_key' => File.read(File.expand_path('~/Dropbox/Factor/Dev/chef/factor-validator.pem')),
-        'organization' => 'factor',
-        'name' => 'node',
-        'runlist' => 'recipe[learn_chef_apache2]'
-      }
+      it ':: bootstrap' do
 
-      @service_instance.test_action('bootstrap',params) do
-        expect_info message: 'Setting up private key'
-        expect_info message: 'Running setup commands'
-        expect_info message: 'Uploading /etc/chef/validation.pem'
-        expect_info message: 'Uploading /etc/chef/client.rb'
-        expect_info message: 'Running chef bootstrap commands'
-        expect_return
+        bootstrap_params = {
+          'host'    => ENV['CHEF_TEST_HOST'],
+          'name'    => ENV['CHEF_TEST_NAME'],
+          'runlist' => ENV['CHEF_TEST_RUNLIST']
+        }
+
+        params = @params.merge(bootstrap_params)
+
+        @service_instance.test_action('bootstrap',params) do
+          expect_return
+        end
       end
     end
   end
